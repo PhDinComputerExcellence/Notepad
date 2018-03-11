f80b1a914e65f2417bb9c19b8843bf6e6d7937da
PhDinComputerExcellence <kevinleela@hotmail.com>
2018-03-11 14:19:39 -0700

    Stuff

---
+++ src/JFontChooser.java
@@ -0,0 +1,261 @@
+/** 
+ *   Name:  Lee, Kevin
+ *   Project: 2
+ *   Due:      3/7/2018
+ *   Course:   CS-245-01-w18  
+ * 
+ *   Description: 
+ *    A Font chooser Dialog that can be used anytime to output a color and a font.
+ */ 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.font.TextAttribute;
+import java.text.AttributedString;
+
+import javax.swing.text.StyleConstants;
+
+import javax.swing.*;
+
+public class JFontChooser extends JDialog implements ActionListener {
+	/**
+	 * 
+	 */
+
+	
+	JPanel topPanel;
+	JPanel botPanel;
+	
+	JPanel topLeft;
+	JPanel top;
+	JPanel topRight;
+	
+	JLabel mainLabel;
+	
+	JTextField text1;
+	JTextField text2;
+	JTextField text3;
+	JTextField maintext;
+	
+	JButton colorchooser;
+	JButton exit;
+	
+	JList<String> list;
+	JList<String> styleList;
+	JList<String> size;
+	
+	JLabel label;
+	
+	String fontname;
+	Font font;
+	Color color;
+	int style;
+	boolean chosen;
+	public JFontChooser() {
+		chosen = false;
+		String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		setSize(800, 550);
+		setTitle("JFontChooser");
+		color = Color.BLACK;
+		
+		setResizable(false);
+		setLayout(new GridLayout(2,1));
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		text1 = new JTextField();
+		text2 = new JTextField();
+		text3 = new JTextField();
+		
+		topPanel = new JPanel(new BorderLayout());
+		topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		topLeft = new JPanel();
+		
+		topLeft.setLayout(new BoxLayout(topLeft, BoxLayout.PAGE_AXIS));
+		top = new JPanel();
+		top.setLayout(new BoxLayout(top, BoxLayout.PAGE_AXIS));
+		topRight = new JPanel();
+		topRight.setLayout(new BoxLayout(topRight, BoxLayout.PAGE_AXIS));
+		
+		
+		botPanel = new JPanel();
+		botPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		botPanel.setLayout(new BoxLayout(botPanel, BoxLayout.PAGE_AXIS));
+		
+		DefaultListModel<String> model = new DefaultListModel<>();
+		for (int i = 0; i < fonts.length; i++) {
+			model.addElement(fonts[i]);
+		}
+		list = new JList<>(model);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//		list.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+		JScrollPane thelist = new JScrollPane(list);
+		list.setSelectedIndex(0);
+		
+		DefaultListModel<String> model1 = new DefaultListModel<>();
+		model1.addElement("Plain");
+		model1.addElement("Bold");
+		model1.addElement("Italics");
+		model1.addElement("Italics+Bold");
+		styleList = new JList<>(model1);
+		styleList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//		styleList.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+		JScrollPane thelist2 = new JScrollPane(styleList);
+		styleList.setSelectedIndex(0);
+		
+		DefaultListModel<String> model2 = new DefaultListModel<>();
+		model2.addElement("8");
+		model2.addElement("9");
+		model2.addElement("10");
+		model2.addElement("11");
+		model2.addElement("12");
+		model2.addElement("14");
+		model2.addElement("16");
+		model2.addElement("18");
+		model2.addElement("20");
+		model2.addElement("22");
+		model2.addElement("24");
+		model2.addElement("26");
+		model2.addElement("28");
+		model2.addElement("36");
+		model2.addElement("48");
+		model2.addElement("72");
+		size = new JList<>(model2);
+		size.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//		size.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+		JScrollPane thelist3 = new JScrollPane(size);
+		size.setSelectedIndex(0);
+		
+		JLabel label = new JLabel("Font : ", JLabel.LEFT);
+		label.setBorder(BorderFactory.createEmptyBorder(0,0,0,100));
+		topLeft.add(label);
+		text1.setMaximumSize(new Dimension(3000,10));;
+		topLeft.add(text1);
+		topLeft.add(thelist);
+		
+		top.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+		JLabel label2 = new JLabel("Style : ", JLabel.LEFT);
+		label2.setBorder(BorderFactory.createEmptyBorder(0,0,0,100));
+		top.add(label2);
+		text2.setMaximumSize(new Dimension(3000, 10));
+		top.add(text2);
+		top.add(thelist2);
+		
+		
+		JLabel label3 = new JLabel("Size : ", JLabel.LEFT);
+		label3.setBorder(BorderFactory.createEmptyBorder(0,0,0,100));
+		topRight.add(label3);
+		text3.setMaximumSize(new Dimension(3000, 10));
+		topRight.add(text3);
+		topRight.add(thelist3);
+		
+		
+		mainLabel = new JLabel("", JLabel.CENTER);
+		mainLabel.setMinimumSize(new Dimension(3000, 70));
+		mainLabel.setMaximumSize(new Dimension(3000, 70));
+		mainLabel.setBorder(BorderFactory.createLineBorder(Color.gray, 1));
+		
+		maintext = new JTextField();
+		maintext.setMaximumSize(new Dimension(3000, 20));
+		
+		
+		topPanel.add(topLeft, BorderLayout.WEST);
+		topPanel.add(top, BorderLayout.CENTER);
+		topPanel.add(topRight, BorderLayout.EAST);
+		botPanel.add(mainLabel);
+		botPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		botPanel.add(maintext);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+		buttonPanel.setBorder(BorderFactory.createEmptyBorder(100, 10, 10, 10));
+		colorchooser = new JButton("Color");
+		colorchooser.addActionListener(this);
+		exit = new JButton("Ok");
+		exit.addActionListener(this);
+		buttonPanel.add(Box.createHorizontalGlue());
+		buttonPanel.add(colorchooser);
+		buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		buttonPanel.add(exit);
+		
+		botPanel.add(buttonPanel);
+		
+		add(topPanel);
+		add(botPanel);
+		
+		new Timer(100, ae-> {
+			String fontname = list.getSelectedValue();
+			int choice = 0;
+			switch (styleList.getSelectedValue()) {
+			case "Bold":
+				choice = 1;
+				break;
+			case "Italics":
+				choice = 2;
+				break;
+			case "Plain":
+				choice = 0;
+				break;
+			case "Italics+Bold":
+				choice = 3;
+				break;
+			}
+			style = choice;
+			font = new Font(fontname,choice, Integer.parseInt(size.getSelectedValue()));
+			mainLabel.setFont(font);
+			mainLabel.setForeground(color);
+			if (!maintext.getText().isEmpty()) {
+			mainLabel.setText(maintext.getText());}
+			else {
+				mainLabel.setText("aAbBcC");
+			}
+		}).start();
+		
+		
+	}
+	
+	public void setDefault(Font x) {
+		font = x;
+	}
+	
+	public void setDefault(Color x) {
+		color = x;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public Font getFont() {
+		return font;
+	}
+	
+	public boolean showDialog(JFrame parent) {
+		setLocationRelativeTo(parent);
+		setModal(true);
+		setVisible(true);
+		if (chosen) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getSource()==colorchooser) {
+			color = JColorChooser.showDialog(this,
+		               "Font Colors", color);
+		}else if (ae.getSource().equals(exit)) {
+			chosen = true;
+			dispose();
+		}
+		
+	}
+
+}
--- src/JNotepad.java
+++ src/JNotepad.java
@@ -1,5 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,6 +12,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.undo.UndoManager;
 public class JNotepad {
 
 	public static void main(String[] args) {
@@ -23,22 +30,47 @@ public class JNotepad {
 
 class Notepad implements ActionListener{
 	JFrame frame;
-	JMenuItem New, open, save, saveas, exit;
+	JMenuItem New, open, save, saveas, exit, PageSetup, Print, Undo, Cut, Copy, Paste, Delete,
+	Find, FindNext, Replace, Goto, SelectAll, TimeDate, WordWrap, Font, StatusBar, ViewHelp, About;
 	JTextArea text;
+	PrinterJob pj;
+	UndoManager un;
 	boolean modified = false;
+	String dir;
 	public Notepad() {
-		frame= new JFrame();
-		frame.setTitle("Notepad");
-		text = new JTextArea();
-		JScrollPane textscroll = new JScrollPane(text);
+		dir = "";
 		
+		frame= new JFrame();
+		frame.setTitle("Untitled - Notepad");
+		text = new JTextArea();
+		text.getDocument().addDocumentListener(new DocumentListener() {
+	        @Override
+	        public void removeUpdate(DocumentEvent e) {
+	        	modified = true;
+	        }
+	        @Override
+	        public void insertUpdate(DocumentEvent e) {
+	        	modified = true;
+	        }
+	        @Override
+	        public void changedUpdate(DocumentEvent arg0) {
+	        	modified = true;
+	        }
+		});
+		JScrollPane textscroll = new JScrollPane(text);
+		PrinterJob pj = PrinterJob.getPrinterJob();
+		un = new UndoManager();
+		text.getDocument().addUndoableEditListener(un);
+
 		frame.add(textscroll);
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-		
+
 		JMenuBar menubar = new JMenuBar();
 		JMenu file = new JMenu("File");
+		JMenu Help = new JMenu("Help");
+
 		New = new JMenuItem("New");
 		New.addActionListener(this);
 		open = new JMenuItem("Open");
@@ -47,38 +79,171 @@ class Notepad implements ActionListener{
 		save.addActionListener(this);
 		saveas = new JMenuItem("Save As");
 		saveas.addActionListener(this);
+		PageSetup = new JMenuItem("Page Setup");
+		PageSetup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				PageFormat pf = pj.pageDialog(pj.defaultPage());
+			}
+		});
+		Print = new JMenuItem("Print");
+		Print.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (pj.printDialog()) {
+			        try {pj.print();}
+			        catch (PrinterException exc) {
+			           
+			         }
+			     }   
+			}
+		}
+		);
+		Undo = new JMenuItem("Undo");
+		Undo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				un.undo();
+			}
+		});
+		Cut = new JMenuItem("Cut");
+		Cut.addActionListener(this);
+		Copy = new JMenuItem("Copy");
+		Copy.addActionListener(this);
+		Paste = new JMenuItem("Paste");
+		Paste.addActionListener(this);
+		Delete = new JMenuItem("Delete");
+		Delete.addActionListener(this);
+		Find = new JMenuItem("Find...");
+		Find.addActionListener(this);
+		FindNext = new JMenuItem("Find Next");
+		FindNext.addActionListener(this);
+		Replace = new JMenuItem("Replace");
+		Replace.addActionListener(this);
+		Goto = new JMenuItem("Go To...");
+		Goto.addActionListener(this);
+		SelectAll = new JMenuItem("Select All");
+		SelectAll.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				text.selectAll();
+			}
+		});
+		TimeDate = new JMenuItem("Time and Date");
+		TimeDate.addActionListener(this);
+		WordWrap = new JMenuItem("Word Wrap");
+		WordWrap.addActionListener(this);
+		Font = new JMenuItem("Font...");
+		Font.addActionListener(this);
+		StatusBar = new JMenuItem("Status Bar");
+		StatusBar.addActionListener(this);
+		ViewHelp = new JMenuItem("View Help");
+		ViewHelp.addActionListener(this);
+		About = new JMenuItem("About JNotepad");
+		About.addActionListener(this);
+
+
+
+
 		file.add(New);
+		New.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_N, 
+		        java.awt.Event.CTRL_MASK));
+		New.setMnemonic('N');
 		file.add(open);
+		open.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_O, 
+		        java.awt.Event.CTRL_MASK));
 		file.add(save);
+		save.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_S, 
+		        java.awt.Event.CTRL_MASK));
 		file.add(saveas);
 		file.addSeparator();
+		file.add(PageSetup);
+		PageSetup.setMnemonic('u');
+		file.add(Print);
+		Print.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_P, 
+		        java.awt.Event.CTRL_MASK));
+		file.addSeparator();
 		exit = new JMenuItem("Exit");
+		exit.setMnemonic('x');
 		exit.addActionListener(this);
 		file.add(exit);
-		
+
 		JMenu edit = new JMenu("Edit");
-		
-		
+		edit.add(Undo);
+		Undo.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_Z, 
+		        java.awt.Event.CTRL_MASK));
+		edit.addSeparator();
+		edit.add(Cut);
+		Cut.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_X, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(Copy);
+		Copy.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_C, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(Paste);
+		Paste.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_V, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(Delete);
+		Delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+		edit.addSeparator();
+		edit.add(Find);
+		Find.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_F, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(FindNext);
+		edit.add(Replace);
+		Replace.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_H, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(Goto);
+		Goto.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_G, 
+		        java.awt.Event.CTRL_MASK));
+		edit.addSeparator();
+		edit.add(SelectAll);
+		SelectAll.setAccelerator(KeyStroke.getKeyStroke(
+		        java.awt.event.KeyEvent.VK_A, 
+		        java.awt.Event.CTRL_MASK));
+		edit.add(TimeDate);
+		TimeDate.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, 0));
+
+
 		JMenu format = new JMenu("Format");
-		
-		
+		format.add(WordWrap);
+		WordWrap.setMnemonic('W');
+		format.add(Font);
+		Font.setMnemonic('F');
+
 		JMenu view = new JMenu("View");
+		view.add(StatusBar);
 		
-		
+		Help.add(ViewHelp);
+		ViewHelp.setMnemonic('H');
+		Help.addSeparator();
+		Help.add(About);
+
 		menubar.add(file);
+		file.setMnemonic('F');
 		menubar.add(edit);
+		edit.setMnemonic('E');
 		menubar.add(format);
+		format.setMnemonic('o');
 		menubar.add(view);
-		
-		
-		
+		view.setMnemonic('V');
+		menubar.add(Help);
+		Help.setMnemonic('H');
+
+
 		frame.setJMenuBar(menubar);
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		frame.setVisible(true);
 	}
 
@@ -86,7 +251,39 @@ class Notepad implements ActionListener{
 	public void actionPerformed(ActionEvent ae){
 		if (ae.getSource()==New) {
 			if (modified) {
-				
+				int opt =JOptionPane.showConfirmDialog(frame, "Do you want to saved your current file?", "Warning!",JOptionPane.YES_NO_CANCEL_OPTION);
+				switch(opt){
+                case JOptionPane.YES_OPTION:
+                    if (dir=="") {
+                    	try {
+							save(false);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                    } else {
+                    	try {
+							save(true);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                    }
+                    break;
+                case JOptionPane.NO_OPTION:
+                	frame.setTitle("Untitled - Notepad");
+    				text.setText("");
+    				modified = false;
+                    break;
+                case JOptionPane.CLOSED_OPTION:
+                    break;
+                case JOptionPane.CANCEL_OPTION:
+                    break;
+
+            }
+			} else {
+				frame.setTitle("Untitled - Notepad");
+				text.setText("");
 			}
 		} else if (ae.getSource()==exit) {
 			frame.dispose();
@@ -98,8 +295,11 @@ class Notepad implements ActionListener{
 				int value = filechoose.showOpenDialog(null);
 				if (value == JFileChooser.APPROVE_OPTION) {
 					File file = filechoose.getSelectedFile();
+					dir = filechoose.getSelectedFile().getAbsolutePath();
 					try {
 						fileRead(file);
+						frame.setTitle(filechoose.getSelectedFile().getName() + " - Notepad");
+						modified = false;
 					} catch (FileNotFoundException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -107,20 +307,49 @@ class Notepad implements ActionListener{
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
+
 				}
 			}
 		} else if(ae.getSource()==saveas) {
 			try {
-				save();
+				save(false);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}else if (ae.getSource()==save){
+			if (dir=="") {
+				try {
+					save(false);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					save(true);
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 		
+		
+		else if (ae.getSource()==Cut) {
+			text.cut();
+			text.updateUI();
+		}else if (ae.getSource()==Copy) {
+			text.copy();
+			text.updateUI();
+		}else if (ae.getSource()==Paste) {
+			text.paste();
+			text.updateUI();
+		}
+
 	}
-	
+
 	public void fileRead(File x) throws IOException {
 		text.setText("");
 		BufferedReader read= new BufferedReader(new FileReader(x));
@@ -131,14 +360,14 @@ class Notepad implements ActionListener{
 			text.append("\n");
 		}
 		read.close();
-		
+
 	}
-	
-	public void save() throws IOException {
+
+	public void save(boolean alreadyexists) throws IOException {
 		Boolean exists = true;
 		JFileChooser chooser = new JFileChooser();
 		chooser.setCurrentDirectory(new File("/home/me/Documents"));
-		while (exists) {
+		while (exists && !alreadyexists) {
 			int option = chooser.showSaveDialog(null);
 			if (option == JFileChooser.APPROVE_OPTION) {
 				String hold;
@@ -155,28 +384,56 @@ class Notepad implements ActionListener{
 	                    break;
 	                case JOptionPane.CANCEL_OPTION:
 	                    break;
-	                    
+
 	            }
 				}
 				if (!exists) {
+					String newname;
 					if (filetypecheck(chooser.getSelectedFile().getAbsolutePath())){
 						hold = chooser.getSelectedFile().getAbsolutePath();
+						newname = chooser.getSelectedFile().getName();
+						
 					} else {
 						hold = chooser.getSelectedFile().getAbsolutePath() + ".txt";
+						newname = chooser.getSelectedFile().getName()+".txt";
+					}
+
+					FileWriter writer = new FileWriter(hold);
+					dir = hold;
+					writer.write(text.getText());
+					writer.close();
+					modified = false;
+					frame.setTitle(newname + " - Notepad");
+				} else {
+					String newname;
+					if (filetypecheck(chooser.getSelectedFile().getAbsolutePath())){
+						hold = chooser.getSelectedFile().getAbsolutePath();
+						newname = chooser.getSelectedFile().getName();
+						
+					} else {
+						hold = chooser.getSelectedFile().getAbsolutePath() + ".txt";
+						newname = chooser.getSelectedFile().getName()+".txt";
 					}
 					
 					FileWriter writer = new FileWriter(hold);
 					writer.write(text.getText());
 					writer.close();
-				} else {
-					
+					exists =false;
+					modified = false;
+					frame.setTitle(newname + " - Notepad");
 				}
 				} else {
 					exists = false;
 				}
 		}
+		if (alreadyexists) {
+			FileWriter writer = new FileWriter(dir);
+			writer.write(text.getText());
+			writer.close();
+			modified = false;
+		}
 	}
-	
+
 	public boolean filetypecheck(String x) {
 		if (x.substring(x.length()-4, x.length()).equals(".txt")) {
 			return true;
